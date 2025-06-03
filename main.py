5be9710485b6318be10c3b07a49bc01484163e91
Husan Eshonqulov <fazliddinusarov1999@gmail.com>
2025-06-03 17:56:37 +0500

    done

---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import wikipedia
+
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+wikipedia.set_lang("uz")
+
+bot = Bot("7808069346:AAEi4JcH2Okd7v5ulELTm4NxhLEfjq_94-I")
+dp = Dispatcher()
+
+
+def bootstrap():
+    print("bot has been started....")
+
+
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    await message.answer("Welcome to the bot.")
+
+
+@dp.message(Command("wiki"))
+async def wiki_handler(message: Message):
+    title = message.text[6:]
+    summary = wikipedia.summary(title)
+    await message.answer(summary)
+
+
+async def main():
+    dp.startup.register(bootstrap)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
